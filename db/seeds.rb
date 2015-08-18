d79e75b2bac13834e9141822387539db9e62e322
Stephanie Lam <m.stephanielam@gmail.com>
2015-08-18 16:01:50 -0700

    Edited seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,108 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Tasks
 Task.create({
-  description: 'Gym',
-  starts_at: '2014-10-10',
+  description: 'recurring, not braindump',
+  starts_at: '2014-10-10T08:00:00.000Z',
   repeat_times: 5,
-  repeats_every: "day",
-})
+  })
 
-Task.create({
-  description: 'Read',
-  starts_at: '2014-11-10',
-  repeat_times: 1,
-  repeats_every: "day"
-})
-
-Task.create({
-  description: 'Yoga',
-  starts_at: '2014-12-10',
-  repeat_times: 1,
-  repeats_every: "week"
-})
-
-Task.create({
-  description: 'Code',
-  repeat_times: 5,
-  repeats_every: "week"
-})
-
-Task.create({
-  description: 'Buy groceries',
-})
-
-Task.create({
-  description: 'Email boss',
-})
-
-Task.create({
-  description: 'Buy presents for Stephanie',
-  repeat_times: 5,
-  repeats_every: "week"
-})
-
-Task.create({
-  description: 'Feed Stephanie',
-  repeat_times: 50,
-  repeats_every: "week"
-})
-
-# Completed
 Completion.create({
   task_id: 1,
   completed_at: '2014-10-12'
-})
+  })
 
 Completion.create({
   task_id: 1,
-  completed_at: '2014-12-12'
-})
+  completed_at: '2014-10-13'
+  })
 
-Completion.create({
-  task_id: 1,
-  completed_at: '2014-12-12'
-})
-  
+Task.create({
+  description: 'scheduled, not braindump',
+  starts_at: '2014-10-10T12:00:00.000Z',
+  })
+
 Completion.create({
   task_id: 2,
   completed_at: '2014-11-12'
-})
+  })
+
+Task.create({
+  description: 'Salsa Dancing',
+  starts_at: '2014-10-10T17:00:00.000Z',
+  repeat_times: 1,})
 
 Completion.create({
-  task_id: 7,
-  completed_at: '2014-10-14'
-})
+  task_id: 3,
+  completed_at: '2014-12-12'
+  })
 
-Completion.create({
-  task_id: 7,
-  completed_at: '2014-10-15'
-})
-
-Completion.create({
-  task_id: 7,
-  completed_at: '2014-10-16'
-})
-
-Completion.create({
-  task_id: 8,
-  completed_at: '2014-10-12'
-})
-
-Completion.create({
-  task_id: 8,
-  completed_at: '2014-10-14'
-})
-
-Completion.create({
-  task_id: 8,
-  completed_at: '2014-10-17'
-})
-
-Completion.create({
-  task_id: 8,
-  completed_at: '2014-10-20'
-})
-
+Task.create({
+  description: 'this should only show up in braindump'
+  })
