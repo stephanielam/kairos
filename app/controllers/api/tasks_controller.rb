2d62e8f2d1da6b1be65030dc20667e9dc208c427
Caleb Williams <caleb.w27@me.com>
2015-08-17 19:18:48 -0700

    fixed bugs got /api/task working

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -5,5 +5,43 @@ class Api::TasksController < ApplicationController
     completions = Completion.all
     render json: tasks.to_json(include: :completions)
   end
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task
+    else
+      render json: get_resource.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    task = Task.find(params[:id])
+    render json: task
+  end
+
+  def update
+    if tasks.update(task_params)
+      render :show
+    else
+    render json: tasks.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :starts_at, :repeat_times, :repeats_every)
+  end
+
+
+
+
+
   
 end
