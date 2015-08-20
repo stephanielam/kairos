f7bc761d3d9cf86d27a46ca7c41da8f6820c0257
fredericrenken <frederic@renken.com>
2015-08-19 18:11:24 -0700

    made sql query work, progress calculation correct, added sql scopes to rails models

--- app/models/task_instance.rb
+++ app/models/task_instance.rb
@@ -1,3 +1,5 @@
 class TaskInstance < ActiveRecord::Base
   belongs_to :task_model
+
+  scope :completed, -> { where('completed_at IS NOT NULL') }
 end
--- app/models/task_model.rb
+++ app/models/task_model.rb
@@ -1,3 +1,5 @@
 class TaskModel < ActiveRecord::Base
   has_many :task_instances
+
+  scope :repeating, -> { where('repeat_times IS NOT NULL') }
 end
