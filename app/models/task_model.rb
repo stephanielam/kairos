f21f6788a8048b0f1dd2e8f5daecb58a7a77052f
fredericrenken <frederic@renken.com>
2015-08-19 20:37:58 -0700

    commented out validations

--- app/models/task_model.rb
+++ app/models/task_model.rb
@@ -1,8 +1,8 @@
 class TaskModel < ActiveRecord::Base
   has_many :task_instances
 
-  validates :description, presence:true, length: { in: 1..50 } 
-  validates :repeat_times, numericality: true
+  # validates :description, presence:true, length: { in: 1..50 }
+  # validates :repeat_times, numericality: true
 
   scope :repeating, -> { where('repeat_times IS NOT NULL') }
 
