4c96788055b0d98bb97df4f140c247aa7760c544
Caleb Williams <caleb.w27@me.com>
2015-08-19 19:05:23 -0700

    added validations to taskModel and taskInstance

--- app/models/task_instance.rb
+++ app/models/task_instance.rb
@@ -1,5 +1,8 @@
 class TaskInstance < ActiveRecord::Base
   belongs_to :task_model
 
+  validates :id, presence: true, uniqueness: true
+  validates :task_model_id, presence: true
+
   scope :completed, -> { where('completed_at IS NOT NULL') }
 end
--- app/models/task_model.rb
+++ app/models/task_model.rb
@@ -1,9 +1,10 @@
 class TaskModel < ActiveRecord::Base
   has_many :task_instances
-<<<<<<< HEAD
-  validates :description, presence:true
-=======
+
+  validates :id, presence: true, uniqueness: true
+  validates :description, presence:true, length: { in: 2..50 } 
+  validates :repeat_times, numericality: true
 
   scope :repeating, -> { where('repeat_times IS NOT NULL') }
->>>>>>> master
+
 end
