8ab13f2eed2195bf1b5e6e6cde4610678d1ac7ef
fredericrenken <frederic@renken.com>
2015-08-20 14:47:27 -0700

    removed stash from task_models

--- app/models/task_model.rb
+++ app/models/task_model.rb
@@ -1,12 +1,8 @@
 class TaskModel < ActiveRecord::Base
   has_many :task_instances
 
-<<<<<<< HEAD
   # validates :id, presence: true, uniqueness: true
-  # validates :description, presence:true, length: { in: 2..50 } 
-=======
   # validates :description, presence:true, length: { in: 1..50 }
->>>>>>> master
   # validates :repeat_times, numericality: true
 
   scope :repeating, -> { where('repeat_times IS NOT NULL') }
